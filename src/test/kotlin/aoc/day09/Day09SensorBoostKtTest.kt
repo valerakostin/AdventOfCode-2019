9b24e1ca049e163486eb073d6361371c1a803563
Valera Kostin <valera.kostin@gmail.com>
2019-12-09 20:07:37 +0100

    fix test

--- src/test/kotlin/aoc/day09/Day09SensorBoostKtTest.kt
+++ src/test/kotlin/aoc/day09/Day09SensorBoostKtTest.kt
@@ -8,7 +8,6 @@ import kotlin.test.assertEquals
 internal class Day09SensorBoostKtTest {
     @Test
     fun `09,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99 takes no input and produces a copy of itself as output`() {
-        //TODO fix me
         val program = mutableListOf(109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99)
         val computer = Computer(program)
         computer.execute()
