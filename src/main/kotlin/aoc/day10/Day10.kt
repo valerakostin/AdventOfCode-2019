83a57e8b01a495fd2d783136610777d31c8da83d
Valera Kostin <valera.kostin@gmail.com>
2019-12-09 23:49:13 +0100

    add day 10 template

---
+++ src/main/kotlin/aoc/day10/Day10.kt
@@ -0,0 +1,22 @@
+package aoc.day10
+
+import aoc.utils.Utils
+
+fun main() {
+
+    val lines =
+            Utils.linesFromResource("InputDay10.txt")
+    println("Input $lines")
+    val task1 = task1()
+    val task2 = task2()
+
+    println(
+            """
+              Day10
+               Task1: $task1
+               Task2: $task2
+            """.trimIndent())
+}
+
+fun task1() {}
+fun task2() {}
