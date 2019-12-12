69fe0b0d2b56050cc1a808e1788b2b1cdb1dda21
Valera Kostin <valera.kostin@gmail.com>
2019-12-13 00:24:23 +0100

    add day 12 part 1 test

---
+++ src/test/kotlin/aoc/day12/Day12N-BodyProblemTest.kt
@@ -0,0 +1,24 @@
+package aoc.day12
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+internal class Day12Test {
+
+    @Test
+    fun `day 12 part 1 answer is 7471`() {
+        val state = State(
+                listOf(
+                        Item(Pos(1, 2, -9), Vel(0, 0, 0)),
+                        Item(Pos(-1, -9, -4), Vel(0, 0, 0)),
+                        Item(Pos(17, 6, 8), Vel(0, 0, 0)),
+                        Item(Pos(12, 4, 2), Vel(0, 0, 0))))
+
+        var nextStep = state
+        nextStep.print()
+        repeat(1000) {
+            nextStep = nextStep.nextStep()
+        }
+        assertEquals(7471, nextStep.energy())
+    }
+}
