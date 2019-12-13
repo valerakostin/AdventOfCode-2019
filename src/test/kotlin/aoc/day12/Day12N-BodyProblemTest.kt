4f7a03bd111c54fff501613e96f3d5c1213c4aeb
Valera Kostin <valera.kostin@gmail.com>
2019-12-13 21:03:11 +0100

    add day 12 part 2 test

--- src/test/kotlin/aoc/day12/Day12N-BodyProblemTest.kt
+++ src/test/kotlin/aoc/day12/Day12N-BodyProblemTest.kt
@@ -21,4 +21,18 @@ internal class Day12Test {
         }
         assertEquals(7471, nextStep.energy())
     }
+
+
+    @Test
+    fun `day 12 part 2 answer is 7471`() {
+
+      val result = task2 { State(
+               listOf(
+                       Item(Pos(1, 2, -9), Vel(0, 0, 0)),
+                       Item(Pos(-1, -9, -4), Vel(0, 0, 0)),
+                       Item(Pos(17, 6, 8), Vel(0, 0, 0)),
+                       Item(Pos(12, 4, 2), Vel(0, 0, 0)))) }
+
+        assertEquals(376243355967784, result)
+    }
 }
