b9a9d81522808a365114e645913f8d5889da433d
Valera Kostin <valera.kostin@gmail.com>
2019-12-16 20:06:59 +0100

    day 13 add part 2 solution

--- src/main/kotlin/aoc/day13/Day13CarePackage.kt
+++ src/main/kotlin/aoc/day13/Day13CarePackage.kt
@@ -4,10 +4,8 @@ import aoc.utils.Computer
 import aoc.utils.Intcode
 
 fun main() {
-    val program = Computer.ProgramReader.readProgram("InputDay13.txt")
-    println("Input $program")
-    val task1 = task1(program)
-    val task2 = task2()
+    val task1 = task1(Computer.ProgramReader.readProgram("InputDay13.txt"))
+    val task2 = task2(Computer.ProgramReader.readProgram("InputDay13.txt"))
 
     println(
             """
@@ -20,6 +18,8 @@ fun main() {
 data class Loc(val x: Int, val y: Int)
 
 fun Long.isBlock() = this == 2L
+fun Long.isBall() = this == 4L
+fun Long.isPaddle() = this == 3L
 
 fun task1(program: Intcode): Int {
     val computer = Computer(program)
@@ -29,4 +29,40 @@ fun task1(program: Intcode): Int {
     return (0 until size).map { outputs[it * 3 + 2] }.count { it.isBlock() }
 }
 
-fun task2() {}
+fun task2(program: Intcode): Int {
+    val game = Game()
+    return game.play(program)
+}
+
+
+class Game {
+    private var ball: Loc = Loc(0, 0)
+    private var paddle: Loc = Loc(0, 0)
+    private var score: Long = 0
+
+    fun play(program: Intcode): Int {
+        program[0] = 2
+        val c = Computer(program, inputSupplier = ::joystick, resumeOnOutput = true)
+        c.execute()
+        while (!c.isReady()) {
+            val output1 = c.output()
+            c.resume()
+            val output2 = c.output()
+            c.resume()
+            val output3 = c.output()
+
+            if (isScoreOutput(output1, output2))
+                score = output3
+            else if (output3.isBall())
+                ball = Loc(output1.toInt(), output2.toInt())
+            else if (output3.isPaddle())
+                paddle = Loc(output1.toInt(), output2.toInt())
+            c.resume()
+        }
+        return score.toInt()
+    }
+
+    private fun isScoreOutput(output1: Long, output2: Long) = output1 == -1L && output2 == 0L
+
+    private fun joystick(counter: Int) = ball.x.compareTo(paddle.x).toLong()
+}
