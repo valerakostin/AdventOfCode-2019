c43f4995a520c451d24b948c413440ebc0a3c95d
Valera Kostin <valera.kostin@gmail.com>
2019-12-20 21:43:26 +0100

    add day 19 part 1 solution

---
+++ src/main/kotlin/aoc/day19/Day19TractorBeam.kt
@@ -0,0 +1,47 @@
+package aoc.day19
+
+import aoc.utils.Computer
+import aoc.utils.Intcode
+
+fun main() {
+
+    val program = Computer.ProgramReader.readProgram("InputDay19.txt")
+    val task1 = task1(program)
+    val task2 = task2()
+
+    println(
+            """
+              Day19
+               Task1: $task1
+               Task2: $task2
+            """.trimIndent())
+}
+
+fun Intcode.copy(): Intcode {
+    val copy = mutableMapOf<Int, Long>()
+    copy.putAll(this)
+    return copy
+}
+
+fun task1(program: Intcode): Int {
+
+    var counter = 0
+    for (i in 0..49) {
+        for (j in 0..49) {
+            val v = getFieldValue(program, i, j)
+            if (v == 1L) {
+                counter += 1
+            }
+        }
+    }
+    return counter
+}
+
+private fun getFieldValue(program: Intcode, i: Int, j: Int): Long {
+    val copy = program.copy()
+    val computer = Computer(copy, inputSupplier = { if (it == 0) i.toLong() else j.toLong() })
+    computer.execute()
+    return computer.output()
+}
+
+fun task2() {}
